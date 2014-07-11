0063db8546537d4700356a3eb40979232f91a183
Michael Ronquest <ronquest@ccri.com>
2014-07-11 16:39:05 -0400

    Status update.

--- geomesa-core/src/main/scala/geomesa/core/process/knn/KNNQuery.scala
+++ geomesa-core/src/main/scala/geomesa/core/process/knn/KNNQuery.scala
@@ -1,27 +1,57 @@
 package geomesa.core.process.knn
 
 import com.vividsolutions.jts.geom.Geometry
+import geomesa.utils.geotools
+ import geomesa.utils.geotools.Conversions
+
+import scala.collection.mutable
+
+// implicits
 import org.geotools.data.Query
 import org.geotools.data.simple.{SimpleFeatureCollection, SimpleFeatureSource}
 import org.opengis.feature.simple.SimpleFeature
 
-/**
- * Created by ronquest on 7/7/14.
- */
 object KNNQuery {
-  def runNewKNNQuery(source:SimpleFeatureSource, query: Query, numDesired: Int, searchRadius: Double, anFeatureForSearch:SimpleFeature ) ={
+  def runNewKNNQuery(source:SimpleFeatureSource,
+                     query: Query,
+                     numDesired: Int,
+                     searchRadius: Double,
+                     aFeatureForSearch: SimpleFeature )
+  : NearestNeighbors = {
+    import geomesa.utils.geotools.Conversions.RichSimpleFeature
     // setup parameters for the iterative query
+    // I very well may be able to use a Transform to compute the distances instead of a visitor
+    // I may be able to setup a filter too
+    // will need to generate transforms and filters on the fly then
     val distanceCalculator
-    = new GeodeticDistanceCalc(anFeatureForSearch.getDefaultGeometryProperty.getValue.asInstanceOf[Geometry])
+    = new GeodeticDistanceCalc(aFeatureForSearch.getDefaultGeometryProperty.getValue.asInstanceOf[Geometry])
     val distanceVisitor = new GeodeticVisitor(distanceCalculator)
-
-    val geohashPQ   = ???(anFeatureForSearch) // at the beginning of each search, this is the list of geohashes to query
+    // setup the GH iterator here -- it needs to use the search point and the searchRadius
+    val geohashPQ   = NearestGeoHashes(aFeatureForSearch, searchRadius) // at the beginning of each search, this is the list of geohashes to query
     // may need to setup the SF PQ here as well.
-    theKNNResults: SimpleFeatureCollection(source, query, numDesired, searchRadius, geoHashPQ, sfPQ)
+    // Initialize Priority Queue of Geohashes to search
+    val sfPQ = new NearestNeighbors(aFeatureForSearch, numDesired)
+
+
+    // now begin recursion
+    runKNNQuery(source, query, distanceVisitor,  geoHashPQ, sfPQ)
   }
-  def runKNNQuery(source: SimpleFeatureSource, query: Query, distanceVisitor:GeodeticVisitor, searchRadius: Float, kNN: NearestNeighbors) = {
-    kNN match {
-      case null =>
+  def runKNNQuery(source: SimpleFeatureSource,
+                  query: Query,
+                  distanceVisitor:GeodeticVisitor,
+                  ghPQ: Any,
+                  kNN: NearestNeighbors)
+    : NearestNeighbors   = {
+    // add a filter to the ghPQ if we've already found kNN
+    val newghPQ = if (kNN.foundK) ghPQ.withFilter(thing(kNN.maxDistance)) else ghPQ
+    if (!newghPQ.hasNext) kNN // return what we've got
+    else {
+      // generate the new query
+      // maybe generate a new feature filter as well if the kNN.isFull
+      // getFeatures
+      // compute distances if not done already or done below
+      // throw the features into the (a?) priority queue
+      //kNN ++ runKNNQuery
     }
   }
 
--- geomesa-core/src/main/scala/geomesa/core/process/knn/NearestNeighbors.scala
+++ geomesa-core/src/main/scala/geomesa/core/process/knn/NearestNeighbors.scala
@@ -1,27 +1,23 @@
 package geomesa.core.process.knn
 
-import com.vividsolutions.jts.geom.Geometry
-import geomesa.core.process.knn
-import org.geotools.data.simple.SimpleFeatureCollection
-import org.geotools.factory.CommonFactoryFinder
-import org.geotools.feature.collection.SortedSimpleFeatureCollection
-import org.geotools.geometry.jts.JTS
-import org.geotools.referencing.{CRS, GeodeticCalculator}
-import org.geotools.util.NullProgressListener
-import org.opengis.filter.expression.PropertyName
-import org.opengis.filter.sort.{SortOrder, SortBy}
 
-object NearestNeighbors {
-
-      def apply(inputCollection: SimpleFeatureCollection, geodeticDistanceCalc: GeodeticDistanceCalc) = {
-        val mutatedCollection = inputCollection.accepts(knn.geodeticVisitor(geodeticDistanceCalc),new NullProgressListener() )
-        val nearestDistanceSorting =  Array[SortBy]{ff.sort("geodeticDistance", SortOrder.DESCENDING )}
-
-        new NearestNeighbors(inputCollection,)
-      }
-}
+import geomesa.utils.geotools.Conversions.RichSimpleFeature
+import org.opengis.feature.simple.SimpleFeature
+import scala.collection.mutable
 
 
-class NearestNeighbors(inputCollection: SimpleFeatureCollection, geodeticSorting: Array[SortBy]) extends SortedSimpleFeatureCollection(inputCollection: SimpleFeatureCollection, geodeticSorting: Array[SortBy]) {
 
+class NearestNeighbors(val aFeatureForSearch:SimpleFeature, val numDesired: Int ) extends mutable.PriorityQueue[SimpleFeature] {
+  val ord: Ordering[SimpleFeature] = Ordering.by {
+    sf: SimpleFeature => aFeatureForSearch.point.distance(sf.geometry)
+  }
+
+  def foundK: Boolean = !(this.length < numDesired)
+
+  def getLast: SimpleFeature = this.take(numDesired).last
+
+  def maxDistance = if (foundK) aFeatureForSearch.point.distance(getLast.geometry)
+  else aFeatureForSearch.point.distance(this.last.geometry) // this can be refactored
+
+  // should override enqueue to prevent more than k elements from being contained
 }
