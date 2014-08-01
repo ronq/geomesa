c854a36fb787584475480768650606eea868fa90
Michael Ronquest <ronquest@ccri.com>
2014-08-01 11:12:58 -0400

    Minor code reformatting. Also removed println from TouchingGeoHashes.

--- geomesa-core/src/main/scala/geomesa/core/process/knn/TouchingGeoHashes.scala
+++ geomesa-core/src/main/scala/geomesa/core/process/knn/TouchingGeoHashes.scala
@@ -15,18 +15,18 @@ object TouchingGeoHashes {
 
   // generates directions for stepping N,NE,E, etc...
   val shiftPattern = for {
-      i <- Set(-1, 0, 1)
-      j <- Set(-1, 0, 1)
-    } yield new Coordinate(i, j)
+    i <- Set(-1, 0, 1)
+    j <- Set(-1, 0, 1)
+  } yield new Coordinate(i, j)
 
   // generates a set of all touching neighbors
-  def touching(gh:GeoHash): Set[GeoHash] = {
+  def touching(gh: GeoHash): Set[GeoHash] = {
 
     val thisLocVector = gh.getPoint.getCoordinate
 
     val thisPrec = gh.prec
 
-    val thisPrecVector = new Coordinate (GeoHash.longitudeDeltaForPrecision(thisPrec),
+    val thisPrecVector = new Coordinate(GeoHash.longitudeDeltaForPrecision(thisPrec),
       GeoHash.latitudeDeltaForPrecision(thisPrec))
 
     // these are actually coordinate candidates, some may be over the poles or past the antimeridian
@@ -34,37 +34,38 @@ object TouchingGeoHashes {
       jumpUnitVector <- shiftPattern
       newX = thisLocVector.x + (jumpUnitVector.x * thisPrecVector.x)
       newY = thisLocVector.y + (jumpUnitVector.y * thisPrecVector.y)
-    } yield new Coordinate(newX,newY)
+    } yield new Coordinate(newX, newY)
 
     // now process the coordinate candidates, treating both the antimeridian and the polar regions
-    val safeCoords = newCoords.flatMap { generateIDLSafeCoordinates }.flatMap
-                                                      { generatePolarSafeCoordinates(_,thisPrecVector,thisLocVector) }
+    val safeCoords = newCoords.flatMap { generateIDLSafeCoordinates }.
+                               flatMap { generatePolarSafeCoordinates(_, thisPrecVector, thisLocVector) }
     // use the now safe coordinates to generate GeoHashes, removing the seed GeoHash
-
-    val newGH = safeCoords.map{ coord => GeoHash(coord.x,coord.y,thisPrec)}.filter{_ != gh}
-    println {gh.hash + " -- " + newGH.map{_.hash}}
-    newGH
+    safeCoords.map { coord => GeoHash(coord.x, coord.y, thisPrec) }.filter { _ != gh }
   }
 
   // handles cases where the seed geohash is in contact with the antimeridian
-  def generateIDLSafeCoordinates(xyPair: Coordinate): Set[Coordinate]  =
-             if (math.abs(xyPair.x) > 180.0) generateIDLMirrorPair(xyPair) else Set(xyPair)
+  def generateIDLSafeCoordinates(xyPair: Coordinate): Set[Coordinate] =
+    if (math.abs(xyPair.x) > 180.0) generateIDLMirrorPair(xyPair) else Set(xyPair)
+
+  // use symmetry about the antimeridian to find the correct coordinate on the other side
+  def generateIDLMirrorPair(xyPair: Coordinate): Set[Coordinate] = {
+    val newLat = xyPair.y
+    val newLon = xyPair.x + degreesLonTranslation(xyPair.x)
+    Set(new Coordinate(newLon, newLat))
+  }
+
+  // taken from inside  getInternationalDateLineSafeGeometry
+  // FIXME refactor the above to expose the method
+  def degreesLonTranslation(lon: Double): Double = (((lon + 180) / 360.0).floor * -360).toInt
 
   // handles cases where the seed geohash is in contact with a pole
   def generatePolarSafeCoordinates(xyPair: Coordinate,
-                                   precVector:Coordinate,
-                                   startingPair:Coordinate ): Set[Coordinate] =
-             if (math.abs(xyPair.y) > 90.0) polarCap(xyPair, precVector,startingPair) else Set(xyPair)
-
-  // use symmetry about the antimeridian to find the correct coordinate on the other side
-  def generateIDLMirrorPair(xyPair:Coordinate): Set[Coordinate] = {
-      val newLat= xyPair.y
-      val newLon= xyPair.x + degreesLonTranslation(xyPair.x)
-      Set(new Coordinate (newLon,newLat))
-  }
+                                   precVector: Coordinate,
+                                   startingPair: Coordinate): Set[Coordinate] =
+    if (math.abs(xyPair.y) > 90.0) polarCap(xyPair, precVector, startingPair) else Set(xyPair)
 
   // generate a Set of GeoHashes which all touch this pole, at the same precision as the seed
-  def polarCap(xyPair: Coordinate,precVector: Coordinate,startingPair: Coordinate): Set[Coordinate] = {
+  def polarCap(xyPair: Coordinate, precVector: Coordinate, startingPair: Coordinate): Set[Coordinate] = {
     val newLat = startingPair.y
     val begin: Double = -180.0 + (0.5 * precVector.x)
     val end: Double = 180.0 - (0.5 * precVector.x)
@@ -72,10 +73,6 @@ object TouchingGeoHashes {
     val newLons = begin to(end, step)
     newLons.map(newLon => new Coordinate(newLon, newLat)).toSet
   }
-
-  // taken from inside  getInternationalDateLineSafeGeometry
-  // FIXME refactor the above to expose the method
-  def degreesLonTranslation(lon: Double): Double = (((lon + 180) / 360.0).floor * -360).toInt
 }
 
 
--- geomesa-core/src/test/scala/geomesa/core/process/knn/TouchingGeoHashesTest.scala
+++ geomesa-core/src/test/scala/geomesa/core/process/knn/TouchingGeoHashesTest.scala
@@ -1,7 +1,6 @@
 package geomesa.core.process.knn
 
 import geomesa.utils.geohash.GeoHash
-
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
@@ -17,12 +16,14 @@ class TouchingGeoHashesTest extends Specification {
     val lon = -78.4953560
     GeoHash(lon, lat, precision)
   }
+
   def generateSuvaGH = {
     val precision = 10
     val lat = -18.140
     val lon = 178.440
     GeoHash(lon, lat, precision)
   }
+
   def generateMcMurdoGH = {
     val precision = 5
     val lat = -77.842
@@ -33,21 +34,21 @@ class TouchingGeoHashesTest extends Specification {
   "Geomesa TouchingGeoHashes" should {
 
     "find GeoHashes  around Charlottesville, Virginia" in {
-      val touchingByCalculation = TouchingGeoHashes.touching(generateCvilleGH).map{_.hash}
-      val touchingByVisualInspection = List (
-      "dqb0te",
-      "dqb0tf",
-      "dqb0td",
-      "dqb0tu",
-      "dqb0ts",
-      "dqb0w5",
-      "dqb0w4",
-      "dqb0wh")
-      touchingByCalculation.forall{ touchingByVisualInspection.contains } must beTrue
+      val touchingByCalculation = TouchingGeoHashes.touching(generateCvilleGH).map ( _.hash )
+      val touchingByVisualInspection = List(
+        "dqb0te",
+        "dqb0tf",
+        "dqb0td",
+        "dqb0tu",
+        "dqb0ts",
+        "dqb0w5",
+        "dqb0w4",
+        "dqb0wh")
+      touchingByCalculation.forall ( touchingByVisualInspection.contains ) must beTrue
     }
 
     "Correctly treat the antimeridian and find GeoHashes around Suva, Fiji" in {
-      val touchingByCalculation = TouchingGeoHashes.touching(generateSuvaGH).map {_.hash}
+      val touchingByCalculation = TouchingGeoHashes.touching(generateSuvaGH).map ( _.hash )
       val touchingByVisualInspection = List(
         "rv",
         "rg",
@@ -57,23 +58,23 @@ class TouchingGeoHashesTest extends Specification {
         "2j",
         "2h",
         "25")
-      touchingByCalculation.forall { touchingByVisualInspection.contains } must beTrue
+      touchingByCalculation.forall ( touchingByVisualInspection.contains ) must beTrue
     }
 
     "Correctly treat the polar region and the antimeridian and find GeoHashes around McMurdo Station" in {
-      val touchingByCalculation = TouchingGeoHashes.touching(generateMcMurdoGH).map{_.hash}
-      val touchingByVisualInspection = List (
-      "h",
-      "j",
-      "n",
-      "0",
-      "1",
-      "4",
-      "5",
-      "2",
-      "r",
-      "q")
-      touchingByCalculation.forall{ touchingByVisualInspection.contains } must beTrue
+      val touchingByCalculation = TouchingGeoHashes.touching(generateMcMurdoGH).map ( _.hash )
+      val touchingByVisualInspection = List(
+        "h",
+        "j",
+        "n",
+        "0",
+        "1",
+        "4",
+        "5",
+        "2",
+        "r",
+        "q")
+      touchingByCalculation.forall ( touchingByVisualInspection.contains ) must beTrue
     }
   }
 }
